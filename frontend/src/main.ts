e37acf66599c8ac4597ebbb9eb480824b837550e
Adrian <adrianlorenzbarrios@gmail.com>
2025-01-16 14:06:34 +0100

    libreria y API de maps implementadas

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { Routes } from '@angular/router';
+import { provideRouter, Routes } from '@angular/router';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LocationListComponent } from './app/features/locations/components/location-list/location-list.component';
 import { ElementListComponent } from './app/features/elements/components/element-list/element-list.component';
 import { MapComponent } from './app/features/map/components/map/map.component';
+import { importProvidersFrom } from '@angular/core';
 
 const routes: Routes = [
   { path: '', redirectTo: '/locations', pathMatch: 'full' },
@@ -13,5 +16,9 @@ const routes: Routes = [
   { path: 'map', component: MapComponent },
 ];
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(BrowserAnimationsModule, GoogleMapsModule),
+  ],
+}).catch((err) => console.error(err));

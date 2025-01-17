79d1dc6def7364825e8489bb991930b22788262f
Adrian <adrianlorenzbarrios@gmail.com>
2025-01-17 22:51:52 +0100

    Ventana de diálogo de Locations añadida

--- frontend/src/app/features/locations/dialogs/edit-location-dialog/edit-location-dialog.component.ts
+++ frontend/src/app/features/locations/dialogs/edit-location-dialog/edit-location-dialog.component.ts
@@ -1,11 +1,49 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { LocationService } from '../../../../core/services/location.service';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-edit-location-dialog',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './edit-location-dialog.component.html',
   styleUrl: './edit-location-dialog.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class EditLocationDialogComponent { }
+export class EditLocationDialogComponent {
+  form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    public dialogRef: MatDialogRef<EditLocationDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private locationService: LocationService
+  ) {
+    this.form = this.fb.group({
+      name: [data?.name || '', Validators.required],
+      lat: [data?.lat || '', Validators.required],
+      lng: [data?.lng || '', Validators.required],
+    });
+  }
+
+  save(): void {
+    if (this.form.valid) {
+      const location = this.form.value;
+      if (this.data) {
+        this.locationService.updateLocations(this.data.id, location);
+      } else {
+        this.locationService.addLocation(location);
+      }
+      this.dialogRef.close();
+    }
+  }
+}

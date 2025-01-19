667aee18dc278c0738e0beba6336fe960e804228
Adrian <adrianlorenzbarrios@gmail.com>
2025-01-19 12:12:09 +0100

    Implementado el servicio de Elements

--- frontend/src/app/core/services/element.service.ts
+++ frontend/src/app/core/services/element.service.ts
@@ -1,10 +1,39 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ElementService {
+  private elementsSubject = new BehaviorSubject<any[]>([]);
+  elements$ = this.elementsSubject.asObservable();
 
-  constructor() { }
+  private apiUrl = 'http://localhost:3000/api/elements';
 
+  constructor(private http: HttpClient) {}
+
+  getElements(): void {
+    this.http.get<any[]>(this.apiUrl).pipe(
+      tap((elements) => this.elementsSubject.next(elements))
+    ).subscribe();
+  }
+
+  addElement(element: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, element).pipe(
+      tap(() => this.getElements())
+    );
+  }
+
+  updateElement(id: number, updatedElement: any): Observable<any> {
+    return this.http.patch<any>(`${this.apiUrl}/${id}`, updatedElement).pipe(
+      tap(() => this.getElements())
+    );
+  }
+
+  deleteElementById(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/${id}`).pipe(
+      tap(() => this.getElements())
+    );
+  }
 }
